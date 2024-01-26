d80c87aadb6475b87677be00a43765b1fd9e8117
Aviral <36530173+aviral17@users.noreply.github.com>
2024-01-27 03:17:03 +0530

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,91 @@
+from turtle import *
+
+pensize(10)  # or width(10)
+color("blue")
+bgcolor("lightblue")
+#A
+penup()
+goto(-40, -40)
+pendown()
+left(75)
+forward(100)
+right(150)
+forward(100)
+backward(50)
+right(105)
+forward(27)
+penup()
+right(180)
+forward(50) # distance b/w A and next letter
+rt(80)
+bk(50)
+pendown()
+
+#V
+lt(10)
+fd(100)
+lt(150)
+fd(100)
+penup()
+right(80)
+fd(20)
+pendown()
+
+#I
+fd(40)
+bk(20)
+rt(90)
+fd(100)
+rt(90)
+fd(20)
+rt(180)
+fd(40)
+penup()
+fd(20)
+pendown()
+
+# R
+# draw vertical line
+left(90)
+forward(100)
+# draw curve
+right(90)
+circle(-25, 180)
+# draw diagonal line
+right(240)
+forward(60)
+penup()
+#lt(140)
+lt(70)
+fd(20)
+lt(90)
+bk(5)
+pendown()
+
+#A
+rt(20)
+forward(100)
+right(150)
+forward(100)
+backward(50)
+right(105)
+forward(27)
+penup()
+right(180)
+forward(50) # distance b/w A and next letter
+rt(95)
+bk(50)
+lt(90)
+fd(20)
+rt(90)
+pendown()
+
+
+#L
+fd(100)
+lt(90)
+fd(50)
+
+
+
+
